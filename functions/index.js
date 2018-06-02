6db482e3a101d737d633ba6cd520f81d5c5410ba
andyfusniak <andy@andyfusniak.com>
2018-06-02 22:26:15 +0700

    Move to a single function without requiring separate module

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,36 @@
+'use strict';
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const crypto = require('crypto');
 
-exports.createAccount = require('./auth-on-create');
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
+
+console.log('global scope');
+
+exports.createAccount = functions.auth.user().onCreate((event) => {
+  console.log(event);
+  console.log('local scope a');
+
+  return firestore.collection('accounts').doc(event.uid).set({
+    email: event.email,
+    privateApiKey: (function () {
+      let base62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let str = '';
+      // 62^43 > 2^256 so 43 characters is greater than 256 bit entropy
+      for (let b of crypto.randomBytes(43)) {
+        str += base62[b % 62];
+        };
+      return str;
+    })(),
+    displayName: event.displayName,
+    created: admin.firestore.FieldValue.serverTimestamp(),
+    lastModified: admin.firestore.FieldValue.serverTimestamp()
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+});
